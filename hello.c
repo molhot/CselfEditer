57207379941e0173295b8bc40cac13b350f03cd0
molhot <sakata19991214@gmail.com>
2025-10-22 04:18:58 -0700

    qを入力すると終了するように変更

--- hello.c
+++ hello.c
@@ -6,6 +6,11 @@ int main(void){
 
     while (read(STDIN_FILENO, &c, 1) == 1)
     {
+        if (c == 'q')
+        {
+            printf("end input mode, bye : )");
+            return 0;
+        }
         printf("char1の文字は: %c\n", c);
     };
 
