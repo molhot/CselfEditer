756d91bdd81b5ad19ce25f5e0403426b796bbfeb
molhot <sakata19991214@gmail.com>
2025-10-22 04:08:24 -0700

    入力の受付機能を追加

--- hello.c
+++ hello.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
-int main(void){
-    printf("Hello,world!");
+#include <unistd.h>
 
+int main(void){
+    char c;
+
+    while (read(STDIN_FILENO, &c, 1) == 1);
     return 0;
 }
