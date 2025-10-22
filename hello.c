d99e1ebf635efe0ae8886bebaf1bf3a78c9f09a6
molhot <sakata19991214@gmail.com>
2025-10-22 05:01:38 -0700

    ビット管理されている入力機能をオフにする

--- hello.c
+++ hello.c
@@ -1,18 +1,46 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <termios.h>
+#include <ctype.h>
 
-int main(void){
+struct termios orig_termios;
+
+void disableRawMode()
+{
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+
+void enableRawMode()
+{
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    atexit(disableRawMode);
+
+    struct termios raw = orig_termios;
+
+    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+    raw.c_oflag &= ~(OPOST);
+    raw.c_cflag |= (CS8);
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
+int main() {
     char c;
 
-    while (read(STDIN_FILENO, &c, 1) == 1)
-    {
-        if (c == 'q')
-        {
-            printf("end input mode, bye : )");
-            return 0;
-        }
-        printf("char1の文字は: %c\n", c);
-    };
+    enableRawMode();
 
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') 
+    {
+        if (iscntrl(c))
+        {
+            printf("%d\r\n", c);
+        } 
+        else
+        {
+            printf("%d ('%c')\r\n", c, c);
+        }
+    }
     return 0;
 }
